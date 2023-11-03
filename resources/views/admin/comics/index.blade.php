5836ef751446de001732a87e1240facedf6ff74b
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-03 15:09:23 +0100

    implementata modale delete
    implementata modale per l'eliminazione di un fumetto

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -39,9 +39,29 @@
                 <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
                   @csrf
                   @method('DELETE')
-                  <button type="submit" class="btn btn-danger">
+                  <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal_{{$comic->id}}">
                     Delete
                   </button>
+                  <div class="modal fade" id="exampleModal_{{$comic->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">
+                            Eliminazione item {{$comic->id}}
+                          </h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Sicuro di voler elminare <em>{{$comic->title}}</em>?
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="submit" class="btn btn-danger">Delete</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
                 </form>
             </td>
           </tr>
